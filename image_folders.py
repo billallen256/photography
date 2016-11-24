672b0dc1e35559e1250e04cafbd74815743a77a4
Bill Allen <photo.allen@gmail.com>
2016-11-24 15:00:11 +0000

    Seems to be working again.  Actual copy operation disabled.  Need better way to generate sequential suffixes to handle more than 26 conflicts.

--- image_folders.py
+++ image_folders.py
@@ -12,7 +12,8 @@ import sys
 
 logging.basicConfig(level=logging.INFO)
 
-min_datetime = datetime(2015)
+min_datetime = datetime(2015, 1, 1)
+output_prefix = 'WPA'
 
 def determine_capture_time(basename, extensions):
     capture_time = None
@@ -53,19 +54,12 @@ def make_output_dir(full_path):
 
     return full_path
 
-def make_name(orig_name, dt):
-    f, ext = os.path.splitext(orig_name)
-    return 'WPA' + dt.strftime('%Y%m%d%H%M%S') + ext.lower()
+def make_name(dt):
+    return output_prefix + dt.strftime('%Y%m%d%H%M%S')
 
-def copy_file(file_path, to_dir, output_file_name):
-    seq = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    output_paths = ( (to_dir + os.sep + output_file_name + s).strip() for s in seq )
-
-    for possible_path in output_paths:
-        if not os.path.exists(possible_path):
-            logging.info('Copying {0} to {1}'.format(file_path, possible_path))
-            #shutil.copy2(file_path, possible_path)
-            break
+def copy_file(from_path, to_path):
+    logging.info('Copying {0} to {1}'.format(from_path, to_path))
+    #shutil.copy2(file_path, possible_path)
 
 def group_files(files):
     groups = {}
@@ -81,21 +75,46 @@ def group_files(files):
 
     return groups
 
+def transpose_dict(d):
+    ret = {}
+
+    for k, v in d.items():
+        if v not in ret:
+            ret[v] = set()
+
+        ret[v].add(k)
+
+    return ret
+
+def generate_move_ops(output_paths, file_groups):
+    for output_path, basenames in output_paths.items():
+        seq = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        seq_counter = 0
+
+        # sort the basenames to preserve sequencing of files captured in the same second
+        for basename in sorted(basenames):
+            for ext in file_groups[basename]:
+                from_path = basename + ext
+                to_path = (output_path + seq[seq_counter]).strip() + ext
+                yield (from_path, to_path)
+            seq_counter += 1
+
 if __name__ == "__main__":
-    input_dir = sys.argv[1]
-    output_dir = sys.argv[2]
-    files = os.listdir(input_dir)
-    files = ( input_dir + os.sep + f for f in files )
+    input_directory = sys.argv[1]
+    output_directory = sys.argv[2]
+    files = os.listdir(input_directory)
+    files = ( input_directory + os.sep + f for f in files )
     files = ( f for f in files if os.path.isfile(f) )
     file_groups = group_files(files)
 
-    capture_times = { basename:determine_capture_time(basename, extensions) for basename, extensions in file_groups }
-    file_groups = { basename:extensions for basename, extensions in file_groups if capture_times[basename] is not None }
-    output_dirs = { basename:determine_output_dir(output_dir, capture_times[basename]) for basename, _ in file_groups }
+    capture_times = { basename: determine_capture_time(basename, extensions) for basename, extensions in file_groups.items() }
+    file_groups = { basename: extensions for basename, extensions in file_groups.items() if capture_times[basename] is not None }
+    output_dirs = { basename: determine_output_dir(output_directory, capture_times[basename]) for basename in file_groups }
 
-    for basename, extensions in file_groups:
-        directory = make_output_dir(output_dir, capture_times[basename])
-        file_name_map = make_names(basename, extensions, dt)
+    # need to ensure the new filenames containing the capture time don't conflict
+    # within their new output directories
+    output_paths = { basename: output_dirs[basename]+os.sep+make_name(capture_times[basename]) for basename in file_groups }
+    output_paths = transpose_dict(output_paths) # transpose so we can generate the move operations as a reduce
 
-        for old_path, new_path in file_name_map:
-            copy_file(old_path, directory, new_path)
+    for from_path, to_path in generate_move_ops(output_paths, file_groups):
+        copy_file(from_path, to_path)
